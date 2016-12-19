fdc134a20dafedf22758c741d1893ec3e0cfa7b6
jankapunkt <jkuester@uni-bremen.de>
2016-12-19 15:18:16 +0100

    added space jam for tiniest cli version

--- install_remote_app.sh
+++ install_remote_app.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 curl -L https://git.io/ejPSng | /bin/sh
-npm -g install mmongo
+npm install -g mmongo
+npm install -g spacejam
 meteor create remote-app
 cd remote-app
 meteor npm install
